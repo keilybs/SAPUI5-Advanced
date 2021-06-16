3bafca22688a0f43acbf15ad51c3b3d57fffa777
keilybs <keily.bustos@softtek.com>
2021-06-16 04:24:25 +0000

    First commit 04

--- Employees/webapp/controller/MainView.controller.js
+++ Employees/webapp/controller/MainView.controller.js
@@ -1,3 +1,4 @@
+//@ts-nocheck
 sap.ui.define([
 		"sap/ui/core/mvc/Controller"
 	],
@@ -10,6 +11,22 @@ sap.ui.define([
 		return Controller.extend("logaligroup.Employees.controller.MainView", {
 			onInit: function () {
 
-			}
+            },
+            onValidate: function(oEvent){
+                var oInput = oEvent.getSource(),
+                    sValue = oInput.getValue();
+
+                if(oInput.getMaxLength() === sValue.length ){
+                    //oInput.setDescription("OK");
+                    this.byId("labelCountry").setVisible(true);
+                    this.byId("slCountry").setVisible(true);
+                }else{
+                    //Input.setDescription("no OK");
+                    this.byId("labelCountry").setVisible(false);
+                    this.byId("slCountry").setVisible(false);
+
+                }
+
+            }
 		});
 	});
